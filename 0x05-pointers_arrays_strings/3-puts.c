248e212ffded6a264d083cbfcc0a4865d2fa6879
Baharmohamed98 <dailybahar98@outlook.com>
2023-09-18 15:17:06 -0700

    3-puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,7 +6,7 @@
  * Return: void
  */
 
-void _Puts(char *str)
+void _puts(char *str)
 
 {
         for (; *str != '\0'; str++)
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * _Putchar - writes the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: the character to print
  *
  * Return: on success 1.
  * on error, -1 is returned, and error is set approistely.
  */
-int _Putchar(char c)
+int _putchar(char c)
 {
         return (write(1, &c, 1));
 }
