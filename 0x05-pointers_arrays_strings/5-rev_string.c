e260f7a8cb9d79b441e01698d4b0e062560fd771
Baharmohamed98 <dailybahar98@outlook.com>
2023-09-18 16:03:08 -0700

    5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,26 +1,24 @@
-#include "maim.h"
+#include "main.h"
 
 /**
- * rev_string - reverses a string 
- *
- * &s: string poianeter input
- *
- * Return: Nothing
+ * rev_string - Reverses a string
+ * @s: The string to be modified
+ * Return: void
  */
 
 void rev_string(char *s)
-{
-	int l, i;
-	char temp;
 
-	/*find strring length without null char*/
-	for (l = 0; s[l] != '\0'; ++l)
-		;
-	/*swap the string by looping to half the string*/
-	for (i = 0; i < l / 2; i++)
-	{
-		temp = s[l];
-		s[i] = s[l -1 - i]; /* -1 because the array starts from 0*/
-		s[l - 1 - i] = temp;
-	}
+{
+        int len = 0, index = 0;
+        char tmp;
+
+        while (s[index++])
+        len++;
+
+        for (index = len - 1; index >= len / 2; index--)
+        {
+        tmp = s[index];
+        s[index] = s[len - index - 1] = tmp;
+        s[len - index - 1] = tmp;
+        }
 }
